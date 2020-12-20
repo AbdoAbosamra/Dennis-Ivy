0ffdc02699367c8bdf9c3e13ac610aac1e1a83ce
Abdo Abosamra <abdo.abosamra80@gmail.com>
2020-12-20 04:15:16 +0200

    CRUD

--- crm1/accounts/urls.py
+++ crm1/accounts/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('', views.home , name = 'home'),
     path('products/',views.products , name = 'products'),
     path('customer/<int:pk>',views.customer , name = 'customer' ),
-    path('createOrder/' , views.createOrder ,name = 'createOrder')
+    path('createOrder/' , views.createOrder ,name = 'createOrder'),
+    path('UpdateOrder/<int:pk>' , views.UpdateOrder ,name = 'UpdateOrder'),
+    path('deleteOrder/<int:pk>' , views.deleteOrder ,name = 'deleteOrder'),
+
+
 ]
