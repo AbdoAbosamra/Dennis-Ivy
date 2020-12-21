97e28171cc7b1298225871b43654e66840432547
Abdo Abosamra <abdo.abosamra80@gmail.com>
2020-12-21 21:20:01 +0200

    url modifications

--- crm1/accounts/urls.py
+++ crm1/accounts/urls.py
@@ -5,10 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.home , name = 'home'),
     path('products/',views.products , name = 'products'),
-    path('customer/<int:pk>',views.customer , name = 'customer' ),
-    path('createOrder/' , views.createOrder ,name = 'createOrder'),
-    path('UpdateOrder/<int:pk>' , views.UpdateOrder ,name = 'UpdateOrder'),
-    path('deleteOrder/<int:pk>' , views.deleteOrder ,name = 'deleteOrder'),
+    path('customer/<str:pk>',views.customer , name = 'customer' ),
+    path('createOrder/<str:pk>' , views.createOrder ,name = 'createOrder'),
+    path('UpdateOrder/<str:pk>' , views.UpdateOrder ,name = 'UpdateOrder'),
+    path('deleteOrder/<str:pk>' , views.deleteOrder ,name = 'deleteOrder'),
 
 
 ]
